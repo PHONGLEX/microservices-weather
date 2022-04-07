df5a8d4e503ab3a4c59e85df758637f5b73d50cc
PHONG LEX <fonglex@gmail.com>
2022-04-07 17:09:32 +0700

    Add initial deps

---
+++ CloudWeather.Precipitation/Controllers/PrecipitationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CloudWeather.Precipitation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrecipitationController : ControllerBase
+    {
+        
+        private readonly ILogger<PrecipitationController> _logger;
+
+        public PrecipitationController(ILogger<PrecipitationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{zip}")]
+        public string Get(string zip, [FromQuery] int? days)
+        {
+            return zip;
+        }
+    }
+}
