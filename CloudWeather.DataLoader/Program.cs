0f77f4db5abc92551e590ae56f69c4c3d815377b
PHONG LEX <fonglex@gmail.com>
2022-04-07 16:49:53 +0700

    Add project files.

---
+++ CloudWeather.DataLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CloudWeather.DataLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
